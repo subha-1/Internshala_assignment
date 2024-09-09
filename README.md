cde66ff51091f2e459d9d1d59cd3685de7878f97
Subhalaxmi Achary <150272661+subha-1@users.noreply.github.com>
2024-09-09 23:32:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Internshala_assignment
+
+# Lunacal Assignment
+
+A static website as an assignment from Lunacal.
+
+
+## Screenshots
+
+![App Screenshot](https://github.com/sreoshee-17/Lunacal-Assignment/blob/main/assignment.png)
+
+
+## Live Link
+
+https://lunacalassign.netlify.app/
+
---
+++ gallery.jsx
@@ -0,0 +1,71 @@
+import img from './../../assets/image.png'
+import { FaArrowLeft, FaRegQuestionCircle } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
+
+const Gallery = () => {
+    return (
+        <div className="mt-[1.3rem] bg-[rgba(54,60,68,0.6)] py-4 px-10 rounded-2xl shadow-[5px_5px_5px_0_rgba(0,0,0,0.5)] shadow-black relative">
+            <div className='absolute left-[0.9rem] top-[1rem]'>
+          <FaRegQuestionCircle className='text-[#828A94]'/>
+        </div>
+
+        <div className='absolute left-[0.8rem] top-[7rem]'>
+            <div className='grid grid-cols-2 gap-[0.1rem]'>
+              <div className='bg-[#4A4E54] rounded-[1px] w-[0.4rem] h-[0.4rem]'></div>
+              <div className='bg-[#4A4E54] rounded-[1px] w-[0.4rem] h-[0.4rem]'></div>
+              <div className='bg-[#4A4E54] rounded-[1px] w-[0.4rem] h-[0.4rem]'></div>
+              <div className='bg-[#4A4E54] rounded-[1px] w-[0.4rem] h-[0.4rem]'></div>
+              <div className='bg-[#4A4E54] rounded-[1px] w-[0.4rem] h-[0.4rem]'></div>
+              <div className='bg-[#4A4E54] rounded-[1px] w-[0.4rem] h-[0.4rem]'></div>
+            </div>
+        </div>
+
+
+
+        <div className="flex lg:flex-row flex-col lg:gap-0 gap-3 lg:p-0 p-[0.3rem] justify-between items-center mb-4">
+          <button className="bg-[#171717] text-white py-3 px-7 rounded-2xl">
+            Gallery
+          </button>
+          <div className="flex items-center space-x-6">
+            <button className="bg-[#40464D] hover:bg-[#5a5e63] shadow-[inset_0px_2px_2px_0px_rgba(255,255,255,0.2),2px_3px_7px_0_rgba(0,0,0,0.8),-1px_-1px_4px_0_rgba(255,255,254,0.4)] text-[0.7rem] font-semibold text-white py-2 px-4 rounded-full">
+              + ADD IMAGE
+            </button>
+            <button className="bg-[#1D1F22] shadow-[-5px_-4px_10px_0_rgba(255,255,254,0.3),5px_5px_8px_0_rgba(0,0,0,0.7)]  text-[#40464D] p-2 rounded-full">
+              <FaArrowRight />
+            </button>
+            <button className="bg-[#1D1F22] shadow-[-5px_-4px_10px_0_rgba(255,255,254,0.3),5px_5px_8px_0_rgba(0,0,0,0.7)]  text-[#40464D] p-2 rounded-full">
+              <FaArrowLeft />
+            </button>
+          </div>
+        </div>
+  
+        {/* Image Gallery */}
+        <div className="flex lg:flex-row flex-col lg:justify-around justify-center items-center lg:gap-0 gap-4 lg:space-x-4  mt-8 ">
+          <div className="lg:w-[10rem] lg:h-[10rem] rounded-2xl">
+            <img
+              src={img}
+              alt="Gallery item"
+              className="object-cover w-full h-full rounded-2xl"
+            />
+          </div>
+          <div className="lg:w-[10rem] lg:h-[10rem] rounded-2xl">
+            <img
+              src={img}
+              alt="Gallery item"
+              className="object-cover w-full h-full rounded-2xl"
+            />
+          </div>
+          <div className="lg:w-[10rem] lg:h-[10rem] rounded-2xl">
+            <img
+              src={img}
+              alt="Gallery item"
+              className="object-cover w-full h-full rounded-2xl"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+export default Gallery;
